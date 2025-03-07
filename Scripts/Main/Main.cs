1585f9522ad724642b9cdffe8b54acea9bcce3c2
aniran <aniran530@gmail.com>
2025-03-07 22:00:15 +0530

    Add highlight tile functionality and update main scene structure

--- Scripts/Main/Main.cs
+++ Scripts/Main/Main.cs
@@ -8,15 +8,23 @@ public partial class Main : Node2D
     // node references
     private Sprite2D _cursorNode;
     private Button _placeBuildingButtonNode;
+    private TileMapLayer _highLightTileMapLayerNode;
 
     // scene references
     private PackedScene _buildingScene;
 
+    // run time constants
+     private static readonly int RADIUS = 3;
+
+    // variables
+    private Vector2? _hoveredGridCell;               // to denote the active cell over which mouse is being hovered
+
     public override void _Ready()
     {
         _buildingScene = GD.Load<PackedScene>( "res://Scenes/Building/Building.tscn" );
-        _cursorNode = GetNode<Sprite2D>("%Cursor");
+        _cursorNode = GetNode<Sprite2D>("Cursor");
         _placeBuildingButtonNode = GetNode<Button>("PlaceBuildingButton");
+        _highLightTileMapLayerNode = GetNode<TileMapLayer>("HighLightTileMapLayer");
         ConnectSignals();
         _cursorNode.Visible = false;
     
@@ -26,6 +34,12 @@ public partial class Main : Node2D
     {
         Vector2 gridPosition = GetMouseGridCellPosition();        
         _cursorNode.GlobalPosition = gridPosition * Globals.GRID_SIZE;
+
+        if ( _cursorNode.Visible && ( !_hoveredGridCell.HasValue || _hoveredGridCell.Value != gridPosition ) ) 
+        {
+            _hoveredGridCell = gridPosition;
+        }
+        UpdateHighLightTileMapLayer();
 //        GD.Print( gridPosition );
     }
 
@@ -34,7 +48,7 @@ public partial class Main : Node2D
         _placeBuildingButtonNode.Pressed += OnButtonPressed;
     }
 
-    public override void _Input(InputEvent evt)
+    public override void _UnhandledInput(InputEvent evt)
     {
         if ( _cursorNode.Visible == true && evt.IsActionPressed( "left_click" ) ) 
         {
@@ -58,10 +72,32 @@ public partial class Main : Node2D
 
         Vector2 gridPosition = GetMouseGridCellPosition();
         building.GlobalPosition = gridPosition * Globals.GRID_SIZE;
+
+        _hoveredGridCell = null;
+        UpdateHighLightTileMapLayer();
     }
 
     private void OnButtonPressed()
     {
         _cursorNode.Visible = true;
+    }
+
+    private void UpdateHighLightTileMapLayer() 
+    {
+        _highLightTileMapLayerNode.Clear();
+        if ( !_hoveredGridCell.HasValue ) 
+        {
+            return;
+        }
+        else 
+        {
+            for ( var x = _hoveredGridCell.Value.X - RADIUS; x <= _hoveredGridCell.Value.X + RADIUS; x++ ) 
+            {
+                for ( var y = _hoveredGridCell.Value.Y - RADIUS; y <= _hoveredGridCell.Value.Y + RADIUS; y++ ) 
+                {
+                    _highLightTileMapLayerNode.SetCell(new Vector2I( (int)x, (int)y ), 0, Vector2I.Zero);
+                }
+            }
+        }
     }
 }
